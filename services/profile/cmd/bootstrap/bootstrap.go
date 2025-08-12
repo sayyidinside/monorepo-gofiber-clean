ac42be38b9795a220b8625f7f933b3f630126178
sayyidinside <sayyiddzulfikharhanif@gmail.com>
2025-08-13 00:49:31 +0700

    ➕ Create Profile Service Server And Bootstraping

---
+++ services/profile/cmd/bootstrap/bootstrap.go
@@ -0,0 +1,52 @@
+package bootstrap
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/sayyidinside/monorepo-gofiber-clean/services/profile/cmd/worker"
+	"github.com/sayyidinside/monorepo-gofiber-clean/services/profile/domain/repository"
+	"github.com/sayyidinside/monorepo-gofiber-clean/services/profile/domain/service"
+	"github.com/sayyidinside/monorepo-gofiber-clean/services/profile/interfaces/http/handler"
+	"github.com/sayyidinside/monorepo-gofiber-clean/services/profile/interfaces/http/routes"
+	sharedRepo "github.com/sayyidinside/monorepo-gofiber-clean/shared/domain/repository"
+	sharedBootstrap "github.com/sayyidinside/monorepo-gofiber-clean/shared/infrastructure/bootstrap"
+	"github.com/sayyidinside/monorepo-gofiber-clean/shared/infrastructure/redis"
+	"github.com/sayyidinside/monorepo-gofiber-clean/shared/interfaces/http/middleware"
+	"github.com/sayyidinside/monorepo-gofiber-clean/shared/pkg/helpers"
+	"gorm.io/gorm"
+)
+
+func Initialize(app *fiber.App, db *gorm.DB, cacheRedis *redis.CacheClient, lockRedis *redis.LockClient) {
+	// Repositories
+	profileRepo := repository.NewProfileRepository(db)
+	userRepo := sharedRepo.NewUserRepository(db)
+
+	// Service
+	profileService := service.NewProfileService(profileRepo, userRepo, cacheRedis)
+
+	// Handler
+	profileHandler := handler.NewProfileHandler(profileService)
+
+	// Setup handler to send to routes setup
+	handler := &handler.Handlers{
+		ProfileHandler: profileHandler,
+	}
+
+	routes.Setup(app, handler)
+}
+
+func InitApp() *sharedBootstrap.Deps {
+	deps, err := sharedBootstrap.NewDeps()
+	if err != nil {
+		log.Fatalf("Failed to connect to depedency: %v", err)
+	}
+
+	worker.StartLogWorker()
+
+	helpers.InitLogger()
+
+	middleware.InitWhitelistIP()
+
+	return deps
+}
---
+++ services/profile/cmd/server/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"github.com/sayyidinside/monorepo-gofiber-clean/services/profile/cmd/bootstrap"
+	"github.com/sayyidinside/monorepo-gofiber-clean/shared/infrastructure/shutdown"
+	"github.com/sayyidinside/monorepo-gofiber-clean/shared/pkg/helpers"
+)
+
+func main() {
+	depedency := bootstrap.InitApp()
+
+	app := fiber.New(fiber.Config{
+		AppName:                 depedency.Config.AppName,
+		EnableIPValidation:      true,
+		EnableTrustedProxyCheck: true,
+	})
+
+	// Initialize default config
+	app.Use(logger.New())
+
+	// Add Request ID middleware
+	app.Use(requestid.New())
+
+	app.Use(helpers.APILogger(helpers.GetAPILogger()))
+
+	// Recover panic
+	app.Use(helpers.RecoverWithLog())
+
+	app.Use(helpers.ErrorHelper)
+
+	bootstrap.Initialize(app, depedency.DB, depedency.Redis.CacheClient, depedency.Redis.LockClient)
+
+	app.Use(helpers.NotFoundHelper)
+
+	shutdownHandler := shutdown.NewHandler(app, depedency.DB, depedency.Redis).WithTimeout(30 * time.Second)
+
+	go func() {
+		if err := app.Listen(fmt.Sprintf(":%s", depedency.Config.BPort)); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	shutdownHandler.Listen()
+}
